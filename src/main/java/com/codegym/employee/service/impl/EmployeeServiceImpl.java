a178859bb73420a76ebb128d76ba6c59ebc93b7c
Khanh Hoang Nam <chienbinhbattu9xpro@gmail.com>
2018-11-16 09:40:42 +0700

    add service

---
+++ src/main/java/com/codegym/employee/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,40 @@
+package com.codegym.employee.service.impl;
+
+import com.codegym.employee.model.Employee;
+import com.codegym.employee.repository.EmployeeRepository;
+import com.codegym.employee.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public Page<Employee> findAll(Pageable pageable) {
+        return employeeRepository.findAll(pageable);
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void save(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public void remove(int id) {
+        employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<Employee> findAllByFullNameContaining(String fullname, Pageable pageable) {
+        return null;
+    }
+}
