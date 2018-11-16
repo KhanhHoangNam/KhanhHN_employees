3b2000c4a029f296f1d6da000c5c45d5572119f4
Khanh Hoang Nam <chienbinhbattu9xpro@gmail.com>
2018-11-16 10:04:04 +0700

    add create form

--- src/main/java/com/codegym/employee/controller/EmployeeController.java
+++ src/main/java/com/codegym/employee/controller/EmployeeController.java
@@ -1,14 +1,32 @@
 package com.codegym.employee.controller;
 
+import com.codegym.employee.model.Employee;
+import com.codegym.employee.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EmployeeController {
-    @GetMapping("/")
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/create-employee")
     public ModelAndView showCreateForm() {
         ModelAndView modelAndView = new ModelAndView("views/create");
+        modelAndView.addObject("employee", new Employee());
+        return modelAndView;
+    }
+
+    @PostMapping("/create-employee")
+    public ModelAndView saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.save(employee);
+        ModelAndView modelAndView = new ModelAndView("views/create");
+        modelAndView.addObject("employee", employee);
+        modelAndView.addObject("message", "A new employee was created");
         return modelAndView;
     }
 }
