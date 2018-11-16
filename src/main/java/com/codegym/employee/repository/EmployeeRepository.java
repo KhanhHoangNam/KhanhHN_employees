e08c209401ebb03f184b2d40b303aaf1352517dd
Khanh Hoang Nam <chienbinhbattu9xpro@gmail.com>
2018-11-16 09:40:32 +0700

    add repository

---
+++ src/main/java/com/codegym/employee/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.codegym.employee.repository;
+
+import com.codegym.employee.model.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Integer> {
+    Page<Employee> findAllByFullNameContaining(String fullname, Pageable pageable);
+}
